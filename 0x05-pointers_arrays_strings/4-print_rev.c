70277e6523d3a3927d21ef08b300ee1145437fbb
Allan Buckley <allnbuckley@gmail.com>
2023-07-24 18:36:59 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_rev - Prints a string, in reverse
+  * @s: String
+  * Return: Always 0
+  */
+void print_rev(char *s)
+{
+	int longi = '0';
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > '0' 0--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n')
+}
